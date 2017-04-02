102685331773ad0a2b0b8183813c73f30242304a
Anurag Saxena <anuraag.jpr@gmail.com>
2017-04-02 21:53:29 +0530

    Added sb admin layout.

--- Gemfile
+++ Gemfile
@@ -5,6 +5,9 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
+gem 'bootstrap_sb_admin_base_v2'
+
+gem "font-awesome-rails"
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.1'
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,28 @@
 class HomeController < ApplicationController
+
+	layout 'application'
+
+	layout :resolve_layout
+
 	def index
 
 	end	
+
+	def client_dashboard
+
+	end	
+
+	private
+
+	  def resolve_layout
+	    case action_name
+	    when "index"
+	      "application"
+	    when "client_dashboard"
+	      "client"
+	    #else
+	    #  "application"
+	    end
+	  end
+
 end
