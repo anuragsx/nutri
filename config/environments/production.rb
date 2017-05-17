3daf291134b5cea46ff8929c1f242b7a365e7d0f
Anurag Saxena <anuraag.jpr@gmail.com>
2017-05-17 06:52:47 +0530

    Fixed production env file.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -84,6 +84,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.assets.compile = true
-  config.assets.precompile =  ['*.js', '*.css', '*.css.scss','*.js.coffee']
+  config.assets.compile = false
+  #config.assets.precompile =  ['*.js', '*.css', '*.css.scss','*.js.coffee']
+  config.assets.initialize_on_precompile = false
 end
